6c9c7e102536a72ab597b7db5179932623fef32f
Khurramshahzad175 <khurramshahzad2440@gmail.com>
2024-09-04 15:36:53 +0500

    string practice with '''

---
+++ string-prc.py
@@ -0,0 +1,118 @@
+jinnah14_points : str  = ''' "The 14 points of Jinnah"  are the 14 key demands made by Muhammad Ali Jinnah, the founder of Pakistan, in
+his address to the Constituent Assembly of Pakistan on August 11, 1947. These points are as bellow:
+
+        FOURTEEN POINTS OF THE GREAT  "QUAID-E-AZAM"     
+--> Fourteen points of 'Quaid-e-Azam' are as under :-
+(1) Federal Type OF Constitution :
+(2) Provincial Independence :
+(3) Effective  Representation Of Minorities :
+(4) Muslims Representation  In The Central Legislature :
+(5) Seperate  Electorates :
+(6) Protection Of Provinces In Majority :
+(7) Religious Liberty :
+(8) Bill/Resolution About Communities :
+(9) Seperation Of Sindh From Bombay :
+(10) Reform In Balochistan And NWFP :
+(11) Share Of Muslims In Services :
+(12) Religious And Cultural Protection For The Muslims :
+(13) Muslims Representation In Ministries :
+(14) Central Constitution And Agreement Of Provinces : '''
+
+print(jinnah14_points)
+
+my_country : str = ''' My Country name is pakistan. 
+                                                  Pakistan is a country located in Asia.
+                                                                                        It is the world's 5th-most populous country.
+                                                                                                                                    With a population of over 216 million people.
+                                                                                                                                                                                 It has four provinces.
+                                                                                                                                           It is a federal parliamentary republic.
+                                                                                                    It came into  being on August 14, 1947.
+                                                      It is the world's 36th-largest country by area.
+                   It is bordered by India to the east.
+It is most beautiful '''
+print(my_country)
+
+national_poet : str = '''  The national poet of Pakistan is Allama Muhammad Iqbal.
+                                                                                  He was a poet, philosopher, and politician.
+                                                                                                                             He was  born on November 9, 1877, in Sialkot, Punjab, British India.
+                                                                                                                                                                                                 He was a key figure in the Pakistan Movement.
+                                                                                                                                                                                                                                              He is known for his poetry and his role in the creation of Pakistan.
+                                                                                                                                                                                                        He worked as a professor of Philosophy.
+                                                                                                                                             He died on April 21, 1938, in Lahore, Punjab, British India.
+                                                                                              He is remembered as a national hero in Pakistan.
+                               He is considered one of the greatest poets of the Urdu language.
+He is my favourite Personality. '''
+print(national_poet)
+
+wars_of_pakistan : str = ''' (1) August 1947 war :
+                                                 Reason: PAkistan And India Independence from Britian.
+                                                                                                     (2) October 1947 :
+                                                                                                                       Reason: First KAshmir  war begins between india and  Pakistan.
+                                                                                                                                                                                     (3) August 1965 war :
+                                                                                                                               Reason: second Kashmir  war between india and pakistan.
+                                                                                                            (4) Dec  1971 war :
+                                                             Reason:  After war Bangladesh becomes a country.
+                                               (5) 1999 war :
+  Reason: Kargil war between india and pakistan. '''
+print(wars_of_pakistan)
+
+province_of_pakistan : str = '''  (1) Punjab :
+                                             Capital: Lahore
+                                                            Population: 110 million
+                                                                                   Area: 205,344 km2
+                                                                                                    (2) Sindh :
+                                                                                                               capital :  Karachi
+                                                                                                                                 Population: 45 million
+                                                                                                                                                       Area: 140,914 km
+                                                                                                                                                                       (3)  Khyber Pakhtunkhwa :
+                                                                                                                                                                                                capital :  Peshawar
+                                                                                                                                                                          Population: 35 million
+                                                                                                                                                          Area: 74,521 km2
+                                                                                                                                        (4)  Balochista
+                                                                                                                       capital :  Quetta
+                                                                                                 Population: 12 million
+                                                                                Area: 347,190 km2 
+                                                          (5) Gilgit-Baltistan :
+                                          capital :  Gilgit
+                   Population: 1.5 million
+   Area: 72,971 km2 '''
+print(province_of_pakistan)
+
+historical_places: str =  '''  (1) Lahore Fort :
+                                               (2) Badshahi Masjd :
+                                                                  (3) Minar-e-Pakistan :
+                                                                                        (4) Mohenjo-Daro :
+                                                                                                          (5) Taxila :
+                                                                                                                       (6) Rohtas Fort :
+                                                                                                                                        (7) Lahore Museum :
+                                                                                                                 (8) Quaid-e-Azam House :
+                                                                                               (9) Mazar-e-Quaid :
+                                                                         (10) Shalamar Gardens : 
+                                                      (11) Faisal Mosque :
+                                         (12) Harappa :
+                       (13) Baltit  Fort :
+(14) Wagah  Border : '''
+print(historical_places)
+
+languages_of_Pakistan : str = '''   
+(1) 'Urdu' :
+         National language of pakistan.
+(2) 'English' :
+               Official language of pakistan.
+(3) 'Punjabi' :
+               Most spoken language in Punjab.
+(4) 'Sindhi' :
+              Most spoken language in Sindh.
+(5) 'Pashto' :
+              Most spoken language in Khyber Pakhtunkhwa.
+(6) 'Saraiki' :
+               Also spoken in punjab.
+(7) 'Balochi' :
+               Most  spoken language in Balochistan.
+'''
+print(languages_of_Pakistan)
+
+
+
+
+
